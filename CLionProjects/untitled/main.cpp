d19845d4bc0d54306053f93263651d3f8f2386e2
Aapo Kallio <aapo.kallio@gmail.com>
2023-12-06 18:46:50 +0200

    test

--- CLionProjects/untitled/main.cpp
+++ CLionProjects/untitled/main.cpp
@@ -17,7 +17,7 @@ int menu() {
     string input;
     int action;
     while (true) {
-        cout << "Syötä haluamasi toiminto (1 = Varaa huone, 2 = lopeta ohjelman käyttö)" << endl;
+        cout << "Syota haluamasi toiminto (1 = Varaa huone, 2 = lopeta ohjelman kaytto)" << endl;
         cin >> input;
         try{
             action = stoi(input);
@@ -30,8 +30,8 @@ int menu() {
 }
 
 int main() {
-    cout << "Tervetuloa hotelli hevonbärse varausjärjestelmään!" << endl;
-    cout << "Meillä on käynnissä tällä hetkellä kampanja, jossa järjestelmämme arpoo sinulle 0, 10% tai 20% alennuksen!"
+    cout << "Tervetuloa hotelli hevonbarse varausjarjestelmaan!" << endl;
+    cout << "Meilla on kaynnissa talla hetkella kampanja, jossa jarjestelmamme arpoo sinulle 0, 10% tai 20% alennuksen!"
          << endl;
     cout << endl;
 
