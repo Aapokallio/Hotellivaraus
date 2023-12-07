33a3d76dbf6b5fe80e14598f96add7c7cfb95e68
Aapo Kallio <Aapo.Kallio@tuni.fi>
2023-12-07 21:05:34 +0200

    progress

--- CLionProjects/Hotellivaraus/main.cpp
+++ CLionProjects/Hotellivaraus/main.cpp
@@ -10,11 +10,15 @@ struct Room {
     bool available;
 };
 
+int randomizerWithMinMax(int min, int max) { //random generaattori joka arpoo raja-arvojen välisen arvon
+    random_device generator;
+    uniform_int_distribution<int> distribution(min, max);
+    return distribution(generator);
+}
 
-random_device generator;                                           // Määritellään random generaattori hotellihuoneiden määrää varten.
-uniform_int_distribution<int> distribution(40 / 2, 300 / 2); // Jaetaan raja-arvot kahdella.
 
-const int numberOfRooms = distribution(generator) * 2;               // Kerrotaan tulos kahdella saadaksemme parillisen luvun joka kerta.
+const int numberOfRooms = randomizerWithMinMax(20, 150) *
+                          2; // Lähetetään puolet halutusta ja tuplataan lopputulos saadaksemme parillisen luvun
 const int halfRooms = numberOfRooms / 2;
 vector<Room> rooms(numberOfRooms);
 
@@ -23,22 +27,67 @@ void empty() { // Printtaa tyhjä line
     cout << endl;
 }
 
+bool availabilityChecker(int roomtype) {  // checkeri jolla selvitetään onko huonetyyppiä 1 tai 2 vapaana.
+    if (roomtype == 1) {
+        for (int i = 1; i <= halfRooms; i++) {
+            if (rooms[i].available) {
+                return true;
+            }
+        }
+        return false;
+    }
+    if (roomtype == 2) {
+        for (int i = halfRooms + 1; i <= numberOfRooms; i++) {
+            if (rooms[i].available) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+void reservation() {
+    string input;
+
+    while (true) {
+        cout << "Valitse halauamasi toiminto (1 = yhden hengen huone, 2 = kahden hengen huone, 3 = takaisin)" << endl;
+        cin >> input;
+
+        empty();
+        if (input == "1") {
+            if (availabilityChecker(1)) {
+
+            }
+        } else if (input == "2") {
+            availabilityChecker(2);
+        } else if (input == "3") {
+            break;
+        } else {
+            cout << "Virheellinen syote!" << endl;
+            empty();
+        }
+    }
+}
 
 
-int menu() {
+void menu() {
     string input;
     while (true) {
         cout << "Syota haluamasi toiminto (1 = Varaa huone, 2 = lopeta ohjelman kaytto)" << endl;
         cin >> input;
         if (input == "1") {
-
+            reservation();
+        } else if (input == "2") {
+            break;
+        } else {
+            cout << "Virheellinen komento!" << endl;
         }
     }
 }
 
-int main() {
+void createRooms(){
     for (int i = 1; i <= numberOfRooms; ++i) {  // Luodaan huoneet
-        if (i <= (numberOfRooms/2)){
+        if (i <= (numberOfRooms / 2)) {
             rooms[i].price = 100;
             rooms[i].fitsPeople = 1;
             rooms[i].available = true;
@@ -48,8 +97,10 @@ int main() {
             rooms[i].available = true;
         }
     }
+}
 
-
+int main() {
+    createRooms();
     cout << "Tervetuloa hotelli hevonbarse varausjarjestelmaan!" << endl;
     empty();
     cout << "Hotellissamme on " << numberOfRooms << " huonetta, joista puolet yhden ja puolet kahden hengen huoneita."
@@ -58,4 +109,9 @@ int main() {
     cout << "Meilla on kaynnissa talla hetkella kampanja, jossa jarjestelmamme arpoo sinulle 0, 10% tai 20% alennuksen!"
          << endl;
     empty();
+
+    menu();
+
+
+    cout << "Kiitos ja tervetuloa uudestaan!";
 }
